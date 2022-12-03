3ac3ac1003a802bd3559e88148370ea93b137730
Roman Mazeev <roman.mazeev@lightspeedhq.com>
2022-12-03 23:43:01 +0100

    Initial Commit

---
+++ Diffuser/DiffuserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiffuserApp.swift
+//  Diffuser
+//
+//  Created by Roman Mazeev on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DiffuserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
